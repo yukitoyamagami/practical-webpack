6bfbc3a461ffb99fd9e2a879e142a5b72cb1b43a
yukitoyamagami <yukiright1012@gmail.com>
2023-02-07 00:08:26 +0900

    introduce Babel

--- webpack.common.js
+++ webpack.common.js
@@ -29,6 +29,15 @@ module.exports = {
       },
     },
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
