d32757847a33998f00551e82afd96d0dec51c65b
yukitoyamagami <yukiright1012@gmail.com>
2023-02-07 00:29:31 +0900

    Use of Polyfill

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: 3,
+        // debug: true
+      },
+    ],
+  ],
 };
--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,14 @@ $('body')
   .append(result)
   .append(`<p>${greet(`App`)}</p>`);
 velocity($('h1'), 'fadeIn', { duration: 2000, loop: true });
+
+const promise = new Promise((resolve) => {
+  setTimeout(() => resolve('hello!'), 3000);
+});
+
+async function delayHello() {
+  const value = await promise;
+  console.log(value);
+}
+
+delayHello();
