74be4f0429801a77fef6d83887a82ccf69aa9f5a
yukitoyamagami <yukiright1012@gmail.com>
2023-02-07 22:49:07 +0900

    introduce eslint and prettier

--- src/js/modules/greet.js
+++ src/js/modules/greet.js
@@ -1,3 +1,3 @@
 export function greet(name) {
-  return `Hello ${name}!`
+  return `Hello ${name}!`;
 }
--- webpack.common.js
+++ webpack.common.js
@@ -32,6 +32,15 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
